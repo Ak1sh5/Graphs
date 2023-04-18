b48d1e99bc92709c2dc4636b651b8789d39a1447
Levelling-up-everyday <106878532+Levelling-up-everyday@users.noreply.github.com>
2023-04-19 00:38:44 +0530

    Create 14.Os_to_Xs.cpp

---
+++ 14.Os_to_Xs.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+private:
+    void dfs(int row, int col, vector<vector<int>> &vis, 
+    vector<vector<char>> &mat, int delrow[], int delcol[]) {
+        vis[row][col] = 1; 
+        int n = mat.size();
+        int m = mat[0].size();
+        
+        // check for top, right, bottom, left 
+        for(int i = 0;i<4;i++) {
+            int nrow = row + delrow[i];
+            int ncol = col + delcol[i]; 
+            // check for valid coordinates and unvisited Os
+            if(nrow >=0 && nrow <n && ncol >= 0 && ncol < m 
+            &&  mat[nrow][ncol] == 'O'&& vis[nrow][ncol]==0) {
+                dfs(nrow, ncol, vis, mat, delrow, delcol); 
+            }
+        }
+    }
+public:
+    vector<vector<char>> fill(int n, int m, vector<vector<char>> mat)
+    {
+         int delrow[] = {-1, 0, +1, 0};
+        int delcol[] = {0, 1, 0, -1}; 
+        vector<vector<int>>vis(n,vector<int>(m,0));
+       for(int i=0;i<n;i++)
+       {
+           for(int j=0;j<m;j++){
+               if(i==0||j==0||i==n-1||j==m-1){
+                   if(mat[i][j]=='O'){
+                       dfs(i,j,vis,mat,delrow,delcol);
+                   }
+               }
+           }
+       }
+       for(int i = 0;i<n;i++) {
+            for(int j= 0 ;j<m;j++) {
+                if(!vis[i][j] && mat[i][j] == 'O') 
+                    mat[i][j] = 'X'; 
+            }
+        }
+        
+        return mat; 
+    }
+};
+
+int main(){
+    
+    vector<vector<char>> mat{
+        {'X', 'X', 'X', 'X'}, 
+        {'X', 'O', 'X', 'X'}, 
+        {'X', 'O', 'O', 'X'}, 
+        {'X', 'O', 'X', 'X'}, 
+        {'X', 'X', 'O', 'O'}
+    };
+    
+    Solution ob;
+    // n = 5, m = 4
+    vector<vector<char>> ans = ob.fill(5, 4, mat);
+    for(int i = 0;i < 5;i++) {
+        for(int j = 0;j < 4;j++) {
+            cout<<ans[i][j]<<" ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
+
+Output:
+
+X X X X
+X X X X
+X X X X
+X X X X
+X X O O
+
+Time Complexity: O(N) + O(M) + O(NxMx4) ~ O(N x M), For the worst case, every element will be marked as ‘O’ in the matrix, and the DFS function will be called for (N x M) nodes and for every node, we are traversing for 4 neighbors, so it will take O(N x M x 4) time. Also, we are running loops for boundary elements so it will take O(N) + O(M).
+
+Space Complexity ~ O(N x M), O(N x M) for the visited array, and auxiliary stack space takes up N x M locations at max. 
